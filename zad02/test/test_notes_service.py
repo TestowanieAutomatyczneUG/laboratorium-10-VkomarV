673bc158a8fe53768abc3d11fac9ea8f90796b5a
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2022-01-06 13:19:33 +0100

    Update test_notes_service.py

--- zad02/test/test_notes_service.py
+++ zad02/test/test_notes_service.py
@@ -14,3 +14,18 @@ class Test_NotesService(unittest.TestCase):
         mock_add.return_value = note
         added_note = self.notes_service.add(note)
         self.assertEqual(added_note, note)
+
+    @patch("src.NotesStorage.getAllNotesOf")
+    def test_averageOf(self, mock_getAllNotesOf: MagicMock):
+        mock_getAllNotesOf.return_value = [Note("test1", 3.5), Note("test1", 2.0), Note("test1", 4.5)]
+        self.assertEqual(self.notes_service.averageOf("test1"), 3.33)
+
+    @patch("src.NotesStorage.getAllNotesOf")
+    def test_averageOf_None(self, mock_getAllNotesOf: MagicMock):
+        mock_getAllNotesOf.return_value = []
+        self.assertIsNone(self.notes_service.averageOf("test"))
+
+    @patch("src.NotesStorage.clear")
+    def test_clear(self, mock_clear: MagicMock):
+        mock_clear.return_value = None
+        self.assertIsNone(self.notes_service.clear())
